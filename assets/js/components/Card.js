2dfcedb17064614f11ad6976c6fb4342593c3abc
Toavina Sylvianno <toavinaslvianno14@gmail.com>
2024-06-10 01:42:32 +0300

    Ajout des styles pour le responsive

--- assets/js/components/Card.js
+++ assets/js/components/Card.js
@@ -1,25 +1,59 @@
 const drawCard = ( parent, data ) => {
     let prixActuel = data['old-price'] - data['price-reduction']
     let starsHTML = "";
+
+    // On dessine les étoiles nécessaires
     for (let i=0; i<data.etoile; i++)
         starsHTML += `<i class="fa-solid fa-star"></i>`
-    parent.innerHTML += `<div class="card">
-                    <div class="card-image-container">
-                        <img class="card-image" src="${data.image}" alt="">
-                    </div>
-                    <div class="details">
-                        <span class="description">${data.description}</span>
-                        <div class="prices-container">
-                            <span class="old-price">${data['old-price']}</span>
-                            <span class="price-reduction">-${data['price-reduction']}</span>
-                            <span class="actual-price">${prixActuel}</span>
-                        </div>
-                        <div class="stars-container">
-                            ${starsHTML}
-                        </div>
-                    </div>
-                </div>
-        `
+
+    parent.innerHTML += 
+                        `<div class="card">
+                            <div class="card-image-container">
+                                <img class="card-image" src="${data.image}" alt="">
+                            </div>
+                            <div class="details">
+                                <span class="name">${data.name}</span>
+                                <span class="description">${data.description}</span>
+                                <div class="prices-container">
+                                    <span class="old-price">${data['old-price']}€</span>
+                                    <span class="price-reduction">-${data['price-reduction']}€</span>
+                                    <span class="actual-price">${prixActuel}€</span>
+                                </div>
+                                <div class="stars-container">
+                                    ${starsHTML}
+                                </div>
+                            </div>
+                        </div>`
 }
 
-export {drawCard}
+const drawCardCategory = ( parent, data) => {
+    let categoryCard  = document.createElement('div')
+    let categoryName  = document.createElement('span')
+    let categoryImg  = document.createElement('img')
+
+    categoryCard.classList.add('category-card')
+    categoryName.classList.add('category-name')
+    categoryImg.classList.add('category-image')
+
+    categoryImg.src = data.image
+
+    categoryName.textContent = data.categorie
+    categoryCard.append(categoryName, categoryImg)
+    parent.append(categoryCard)
+    // parent.innerHTML += `
+    //                     <div class="category-card" >
+    //                         <span class="category-name">${data.categorie}</span>
+    //                     </div>
+    //                     `
+}
+
+const drawDetailedCard = ( parent , data ) => {
+    parent.innerHTML =  `
+                            <div id="main-left">
+                                <img src="${data.image}" alt="">
+                            </div>
+                            <div id="main-right">                  
+                            </div>
+                        `
+}
+export {drawCard, drawCardCategory, drawDetailedCard}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-import { drawCard } from "./components/Card.js";
+import { drawCard , drawCardCategory, drawDetailedCard} from "./components/Card.js";
 
 fetch('/assets/data/data.json')
     .then ( res => res.json() )
@@ -24,10 +24,83 @@ fetch('/assets/data/data.json')
                 let targets = data[targetName]
                 console.log(targetName);
                 main.innerHTML = `<h1 id="main-title">${targetName.toUpperCase()}</h1>`
-                main.append(cardContainer)
+
+                if ( targets == undefined || targets.length == 0 ||  targets == null) {
+                    main.innerHTML +=  `<div style="text-align:center; font-size:32px; color: #000;">Il n'y a plus de produits disponibles </div>`
+                    return
+                }
+                else main.append(cardContainer)
+
                 console.log(data[targetName]);
                 targets.forEach( target => drawCard(cardContainer, target))
                 
             })
         }
-    } );
+
+        let cardCategoryContainer = document.querySelector('#category-container')
+        console.log(cardCategoryContainer);
+
+        for (let i=0; i<3; i++) {
+            console.log(dataItem[i]);
+            drawCardCategory(cardCategoryContainer , dataItem[i]);
+        }
+
+        let cards = document.querySelectorAll('.card');
+        cards.forEach( card => {
+            card.addEventListener('click', () => {
+                console.log(card);
+
+                let img = card.querySelector('img')
+                let path = img.src
+                
+                let object = {
+                    'image': img
+                }
+
+                drawDetailedCard(main, object)
+            })  
+        })
+
+        let menuToogle  = document.querySelector('#menu-toogle')
+
+        menuToogle.addEventListener('click', () => {
+            let menuContents = document.querySelectorAll('.menu-content')
+
+            // On gere le menu toogle en mode mobile
+            if (menuToogle.classList.contains('desactive')) {
+                menuToogle.classList.remove('desactive')
+                menuToogle.classList.add('active')
+
+                for(let i=0; i<menuContents.length; i++) {
+                    menuContents[i].style.display = "flex"
+                }
+            }
+            else {
+                menuToogle.classList.remove('active')
+                menuToogle.classList.add('desactive')
+
+                for(let i=0; i<menuContents.length; i++) {
+                    menuContents[i].style.display = "none"
+                }
+            }
+        })
+
+        let menuListTitle = document.querySelectorAll('.list-title')
+        console.log(menuListTitle);
+
+        
+        for (let i=0; i<menuListTitle.length; i++) {
+            menuListTitle[i].addEventListener('click', () => {
+                if (parseInt(window.innerWidth) <= 400) {
+                    let parent =  menuListTitle[i].parentNode
+                    let menuList = parent.querySelector('.menu-list')
+
+                    console.log(menuList);
+
+                    if (menuList.style.display === "flex")
+                        menuList.style.display = "none"
+                    else menuList.style.display = "flex"
+                }
+            })
+        }
+    });
